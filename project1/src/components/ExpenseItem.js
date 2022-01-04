d1a82b876bfe175432b68fda86859e44f4121e9f
Hardik1485 <er.shah.hardik@gmail.com>
2022-01-03 18:58:16 -0600

    Lesson 36 : Expressions in JSX

--- project1/src/components/ExpenseItem.js
+++ project1/src/components/ExpenseItem.js
@@ -1,12 +1,15 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem() {
+  const expenseDate = new Date(2022, 12, 31);
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 294.67;
   return (
     <div className="expense-item">
-      <div>Jan 3rd, 2022</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$294.97</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   );
