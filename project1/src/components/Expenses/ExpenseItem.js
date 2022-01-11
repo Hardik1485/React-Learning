29e433d5b279518af96c9a80f99b3384c5382a22
Hardik1485 <er.shah.hardik@gmail.com>
2022-01-10 19:12:34 -0600

    Lesson 49 : Working with 'State'

--- project1/src/components/Expenses/ExpenseItem.js
+++ project1/src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,18 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card.js";
+import { useState } from "react";
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
   const clickHandler = () => {
-    console.log("Clicked..!!");
+    setTitle("test");
   };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
