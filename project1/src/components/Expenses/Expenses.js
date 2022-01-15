f37ec6f80e86d6d1fb323f8219475dcb864b2506
Hardik1485 <er.shah.hardik@gmail.com>
2022-01-15 07:31:36 -0600

    Lesson 60 : Filter added (assignment)

--- project1/src/components/Expenses/Expenses.js
+++ project1/src/components/Expenses/Expenses.js
@@ -1,10 +1,21 @@
+import react, { useState } from "react";
+
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import Card from "../UI/Card";
+import ExpenseFilter from "./ExpenseFilter";
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
   return (
     <Card className="expenses">
+      <ExpenseFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
       <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
