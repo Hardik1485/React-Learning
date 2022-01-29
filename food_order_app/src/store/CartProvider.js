39812ad557720389fd7cfc1d7be5dfb84c1c38f1
Hardik1485 <er.shah.hardik@gmail.com>
2022-01-28 23:00:11 -0600

    Lesson 148 : Making Items Removable

--- food_order_app/src/store/CartProvider.js
+++ food_order_app/src/store/CartProvider.js
@@ -30,6 +30,20 @@ const cartReducer = (state, action) => {
     return { items: updatedItems, totalAmount: updatedTotalAmount };
   }
   if (action.type === "REMOVE") {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+    const existingItem = state.items[existingCartItemIndex];
+    const updatedTotalAmount = state.totalAmount - existingItem.price;
+    let updatedItems;
+    if (existingItem.amount === 1) {
+      updatedItems = state.items.filter((item) => item.id !== action.id);
+    } else {
+      const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+    return { items: updatedItems, totalAmount: updatedTotalAmount };
   }
 
   return defaultCartState;
@@ -46,7 +60,7 @@ const CartProvider = (props) => {
   };
 
   const removeItemFromCartHandler = (id) => {
-    dispatchCartAction({ type: "REMOVE", item: id });
+    dispatchCartAction({ type: "REMOVE", id: id });
   };
 
   const cartContext = {
