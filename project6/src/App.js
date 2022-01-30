f8be4e376992e57ddba6b80de9b2f38164079389
Hardik1485 <er.shah.hardik@gmail.com>
2022-01-29 22:31:10 -0600

    Lesson 178 : Using async/await

--- project6/src/App.js
+++ project6/src/App.js
@@ -5,23 +5,22 @@ import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
-  const fetchMoviesHandler = () => {
-    fetch("https://swapi.dev/api/films")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        const transformedMovies = data.results.map((movieData) => {
-          return {
-            id: movieData.episode_id,
-            title: movieData.title,
-            openingText: movieData.opening_crawl,
-            releaseData: movieData.release_date,
-          };
-        });
-        setMovies(transformedMovies);
-      }); // default method is GET
-  };
+
+  async function fetchMoviesHandler() {
+    const response = await fetch("https://swapi.dev/api/films"); // default method is GET
+    const data = await response.json();
+
+    const transformedMovies = data.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseData: movieData.release_date,
+      };
+    });
+    setMovies(transformedMovies);
+  }
+
   return (
     <React.Fragment>
       <section>
