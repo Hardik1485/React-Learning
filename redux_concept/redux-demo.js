c00f4863cfb669a01190f3ac0758688f732bbb8a
Hardik1485 <er.shah.hardik@gmail.com>
2022-02-19 10:29:23 -0600

    Lesson 230,231 : Core Redux Concept
    npm init -y
    npm install redux

---
+++ redux_concept/redux-demo.js
@@ -0,0 +1,31 @@
+const redux = require("redux"); //NodeJs syntax to import redux
+
+//Reducer function
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === "increment") {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+
+  if (action.type === "decrement") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+
+  return state;
+};
+
+//Create Store
+const store = redux.createStore(counterReducer);
+
+const counterSubscriber = () => {
+  const latestState = store.getState();
+  console.log(latestState);
+};
+
+store.subscribe(counterSubscriber);
+
+store.dispatch({ type: "increment" });
+store.dispatch({ type: "decrement" });
